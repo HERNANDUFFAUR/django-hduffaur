77e3c7a71b2fc6a5286dc8ff954bdfe660793b67
HERNANDUFFAUR <duffaur.hernan@gmail.com>
2022-10-04 03:45:33 -0300

    finalizo entregable

--- proyectoClases/urls.py
+++ proyectoClases/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('mi-template/', views.mi_template),
     path('mi-template/<str:nombre>', views.tu_template),
     path('prueba-template/', views.prueba_template),
+    path('ver-personas/', views.ver_personas),
+    path('crear-persona/<str:nombre>/<str:apellido>/', views.crear_persona),
     path('hola/', views.hola ),
     path('admin/', admin.site.urls),
 ]
--- proyectoClases/views.py
+++ proyectoClases/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.template import Context, Template, loader
 import random
+from home.models import Persona
+
 
 def hola(request):
     return HttpResponse('Buenas Noches Clase 41765! =D ')
@@ -42,4 +44,24 @@ def prueba_template(request):
     template = loader.get_template('prueba_template.html')
     template_renderizado = template.render(mi_contexto)
     
+    return HttpResponse(template_renderizado)
+
+def crear_persona(request,nombre,apellido):
+    persona = Persona(nombre=nombre, 
+                      apellido=apellido, 
+                      edad=random.randrange(10,98), 
+                      fecha_nacimiento=datetime.now())
+    
+    persona.save()
+        
+    template = loader.get_template('crear_persona.html')
+    template_renderizado = template.render({'personas': persona})
+    return HttpResponse(template_renderizado)
+
+def ver_personas(request):
+    personas = Persona.objects.all() 
+    
+    template = loader.get_template('ver_personas.html')
+    template_renderizado = template.render({'personas': personas})
+       
     return HttpResponse(template_renderizado)
