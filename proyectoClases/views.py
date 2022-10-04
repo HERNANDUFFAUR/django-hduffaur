fc0e86c2f5268f74dc66bb18110817ce255e1f9d
HERNANDUFFAUR <duffaur.hernan@gmail.com>
2022-10-04 04:11:45 -0300

    a

--- proyectoClases/views.py
+++ proyectoClases/views.py
@@ -10,41 +10,41 @@ import random
 from home.models import Persona
 
 
-def hola(request):
-    return HttpResponse('Buenas Noches Clase 41765! =D ')
+# def hola(request):
+#     return HttpResponse('Buenas Noches Clase 41765! =D ')
 
-def fecha(request):
-    fecha_y_hora = datetime.now()
-    return HttpResponse(f'la fecha y hora actual es {fecha_y_hora}')
+# def fecha(request):
+#     fecha_y_hora = datetime.now()
+#     return HttpResponse(f'la fecha y hora actual es {fecha_y_hora}')
 
-def calcular_fecha_nacimiento(request, edad):
-    fecha = datetime.now().year - edad
-    return HttpResponse(f'Tu fecha de nacimiento aproximada para tus {edad} años seria :{fecha}')
+# def calcular_fecha_nacimiento(request, edad):
+#     fecha = datetime.now().year - edad
+#     return HttpResponse(f'Tu fecha de nacimiento aproximada para tus {edad} años seria :{fecha}')
 
-def mi_template(request):
-    cargar_archivo = open(r'C:\Users\Duffa\Desktop\CoderHouse\Django\templates\mi_template.html','r')
-    template = Template(cargar_archivo.read())
-    cargar_archivo.close()
-    contexto = Context()
-    template_renderizado = template.render(contexto)
+# def mi_template(request):
+#     cargar_archivo = open(r'C:\Users\Duffa\Desktop\CoderHouse\Django\templates\mi_template.html','r')
+#     template = Template(cargar_archivo.read())
+#     cargar_archivo.close()
+#     contexto = Context()
+#     template_renderizado = template.render(contexto)
     
-    return HttpResponse(template_renderizado)
+#     return HttpResponse(template_renderizado)
 
-def tu_template(request, nombre):
-    template = loader.get_template('tu_template.html')
-    template_renderizado = template.render({'persona': nombre})
+# def tu_template(request, nombre):
+#     template = loader.get_template('tu_template.html')
+#     template_renderizado = template.render({'persona': nombre})
     
-    return HttpResponse(template_renderizado)
+#     return HttpResponse(template_renderizado)
 
-def prueba_template(request):
-    mi_contexto = {'rango': list(range(1,11)),
-                   'valor_aleatorio': random.randrange(1,11)
-                   }
+# def prueba_template(request):
+#     mi_contexto = {'rango': list(range(1,11)),
+#                    'valor_aleatorio': random.randrange(1,11)
+#                    }
     
-    template = loader.get_template('prueba_template.html')
-    template_renderizado = template.render(mi_contexto)
+#     template = loader.get_template('prueba_template.html')
+#     template_renderizado = template.render(mi_contexto)
     
-    return HttpResponse(template_renderizado)
+#     return HttpResponse(template_renderizado)
 
 # def crear_persona(request,nombre,apellido):
 #     persona = Persona(nombre=nombre, 
@@ -67,16 +67,16 @@ def ver_personas(request):
     return HttpResponse(template_renderizado)
 
 def crear_personas(request):
-    persona1 = Persona(nombre='Hernan', 
-                      apellido='Caire', 
+    persona1 = Persona(nombre='Iron', 
+                      apellido='Man', 
                       edad=random.randrange(10,98), 
                       fecha_alta=datetime.now())
-    persona2 = Persona(nombre='Cristian', 
-                      apellido='Castro', 
+    persona2 = Persona(nombre='Chapulín', 
+                      apellido='Colorado', 
                       edad=random.randrange(10,98), 
                       fecha_alta=datetime.now())
-    persona3 = Persona(nombre='Increible', 
-                      apellido='Hulk', 
+    persona3 = Persona(nombre='Batman', 
+                      apellido='I am', 
                       edad=random.randrange(10,98), 
                       fecha_alta=datetime.now())
     
